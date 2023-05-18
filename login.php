aebc0a2c75fc32592ceaf4eff6d46926508735d6
efdalsahin <muhammetefdalsahin@gmail.com>
2023-05-18 16:11:07 +0300

    login yanlış bilgi ve login bilgileri gösterme

--- login.php
+++ login.php
@@ -2,17 +2,51 @@
 session_start();
 
 // Kullanıcı adı ve şifre kontrolleri
-$correctUsername = "e@e";
-$correctPassword = "123";
+$correctUsername = "g201210076@ogr.sakarya.edu.tr";
+$correctPassword = "g201210076";
+$regex = '/^([^\s@]+)@/';
 
 if ($_POST['ehehe'] === $correctUsername && $_POST['password'] === $correctPassword) {
     $_SESSION["loggedin"] = true;
-    $_SESSION["mail"] = $_POST['ehehe'];
+   
+
+    if (preg_match($regex, $_POST['ehehe'], $matches)) {
+      $username = $matches[1];
+      $_SESSION["mail"] =  $username;
+  } 
 
   sleep(1);
   // Giriş başarılı olduğunda otomatik yönlendirme
-  header("Location: index.html");
+  header("Location: girisyap.php");
   exit;
+}else { ?>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Başarısız</title>
+    <link rel="stylesheet" href="Stiller/girisyap.css">
+  </head>
+  <body>
+    <div id="logincontainer">
+      <div>
+    <p>Yanlış Bilgileri girdiniz.</p>
+    <p>Yönlendiriliyorsunuz...</p>
+    
+  
+    </div>
+    </div>
+  </body>
+  </html>
+  <?php 
+    
+    
+
+    
+    header( "Refresh:2; url=girisyap.php", true, 303);
 }
-?>
+    ?>
+
 
